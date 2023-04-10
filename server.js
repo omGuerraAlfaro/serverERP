1a777095d89fd30bbd066fc22f080bd80aa1683e
Omar Guerra Alfaro <om.guerra@duocuc.cl>
2023-04-09 20:10:31 -0400

    new changes

--- server.js
+++ server.js
@@ -55,14 +55,6 @@ app.post('/users', (req, res) => {
     res.send(results);
   });
 });
-
-app.post('/subindicador/agregar', (req, res) => {
-  const { id_subindicador, cod_contable, descripcion, id_indicador } = req.body;
-  connection.query('INSERT INTO cod_cont_subindicador (id_subindicador, cod_contable, descripcion, id_indicador) VALUES (?, ?, ?, ?)', [id_subindicador, cod_contable, descripcion, id_indicador], (err, results) => {
-    if (err) throw err;
-    res.send(results);
-  });
-});
 /* **************************************************************************************************************************************************************** */
 
 /* **************************************************************************************************************************************************************** */
@@ -112,27 +104,20 @@ app.get('/subcategorias/activos', (req, res) => {
   });
 });
 app.get('/indicadores/activos', (req, res) => {
-  connection.query('SELECT * FROM `cod_cont_indicador` WHERE id_subcategoria = 1;', (err, results) => {
+  connection.query('SELECT IND.id_indicador,IND.cod_contable, IND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria WHERE CAT.id_categoria = 1;', (err, results) => {
     if (err) throw err;
     res.send(results);
   });
 });
 app.get('/subindicadores/activos', (req, res) => {
-  connection.query('SELECT SIND.cod_contable, SIND.descripcion FROM cod_cont_subcategoria SC JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria JOIN cod_cont_subindicador SIND ON IND.id_indicador = SIND.id_indicador WHERE SC.id_subcategoria = 1;', (err, results) => {
+  connection.query('SELECT SIND.cod_contable, SIND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria JOIN cod_cont_subindicador SIND ON IND.id_indicador = SIND.id_indicador WHERE CAT.id_categoria = 1;', (err, results) => {
     if (err) throw err;
     res.send(results);
   });
 });
 
-/* Activos POST */
-app.post('/categorias/activos/agregar', (req, res) => {
-  const { id_categoria, cod_contable, descripcion } = req.body;
-  connection.query('INSERT INTO cod_cont_categoria (id_categoria, cod_contable, descripcion) VALUES (?, ?, ?)', [id_categoria, cod_contable, descripcion], (err, results) => {
-    if (err) throw err;
-    res.send(results);/*  */
-  });
-});
-/* Activos SEARCH */
+/* **************************************************************************************************************************************************************** */
+/* Activos SEARCH  AUN NO LO PRUEBO.*/
 app.get('/categorias/activos/search', (req, res) => {
   const { id_categoria, cod_contable, descripcion } = req.body;
   connection.query('SELECT * FROM `cod_cont_categoria` WHERE id_categoria = 1 AND cod_contable LIKE ? AND descripcion LIKE ?', [id_categoria, cod_contable, descripcion], (err, results) => {
@@ -140,18 +125,42 @@ app.get('/categorias/activos/search', (req, res) => {
     res.send(results);
   });
 });
-/* Activos DELETE */
-app.delete('/categorias/activos/delete', (req, res) => {
-  const { id_categoria, cod_contable, descripcion } = req.body;
-  connection.query('DELETE FROM `cod_cont_categoria` WHERE id_categoria = 1 AND cod_contable LIKE ? AND descripcion LIKE ?', [id_categoria, cod_contable, descripcion], (err, results) => {
+/* Activos SUBCATEGORIA */
+app.post('/subcategorias/activos/agregar', (req, res) => {
+  const { id_subcategoria, cod_contable, descripcion, id_categoria } = req.body;
+  connection.query('INSERT INTO cod_cont_subcategoria (id_subcategoria, cod_contable, descripcion, id_categoria) VALUES (?, ?, ?, ?)', [id_subcategoria, cod_contable, descripcion, id_categoria], (err, results) => {
     if (err) throw err;
     res.send(results);
   });
 });
-/* Activos UPDATE */
-app.put('/categorias/activos/update', (req, res) => {
+/* Activos SUBCATEGORIA DELETE */
+app.delete('/subcategorias/activos/eliminar/:id_subcategoria', (req, res) => {
+  const { id_subcategoria } = req.params;
+  connection.query('DELETE FROM `cod_cont_subcategoria` WHERE id_subcategoria = ?', [id_subcategoria], (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+/* Activos SUBCATEGORIA UPDATE */
+app.put('/subcategorias/activos/update', (req, res) => {
   const { id_categoria, cod_contable, descripcion } = req.body;
   connection.query('UPDATE `cod_cont_categoria` SET cod_contable = ?, descripcion = ? WHERE id_categoria = 1 AND cod_contable LIKE ? AND descripcion LIKE ?', [cod_contable, descripcion, id_categoria, cod_contable, descripcion], (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+/* Activos INDICADORES agregar*/
+app.post('/indicadores/activos/agregar', (req, res) => {
+  const { id_indicador, cod_contable, descripcion, id_subcategoria } = req.body;
+  connection.query('INSERT INTO cod_cont_indicador (id_indicador, cod_contable, descripcion, id_subcategoria) VALUES (?, ?, ?, ?)', [id_indicador, cod_contable, descripcion, id_subcategoria], (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+/* Activos INDICADORES DELETE */
+app.delete('/indicadores/activos/eliminar/:id_indicador', (req, res) => {
+  const { id_indicador } = req.params;
+  connection.query('DELETE FROM `cod_cont_indicador` WHERE id_indicador = ?', [id_indicador], (err, results) => {
     if (err) throw err;
     res.send(results);
   });
@@ -173,23 +182,41 @@ app.get('/subcategorias/pasivos', (req, res) => {
   });
 });
 app.get('/indicadores/pasivos', (req, res) => {
-  connection.query('SELECT * FROM `cod_cont_indicador` WHERE id_subcategoria = 2;', (err, results) => {
+  connection.query('SELECT IND.cod_contable, IND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria WHERE CAT.id_categoria = 2;', (err, results) => {
     if (err) throw err;
     res.send(results);
   });
 });
 app.get('/subindicadores/pasivos', (req, res) => {
-  connection.query('SELECT SIND.cod_contable, SIND.descripcion FROM cod_cont_subcategoria SC JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria JOIN cod_cont_subindicador SIND ON IND.id_indicador = SIND.id_indicador WHERE SC.id_subcategoria = 2;', (err, results) => {
+  connection.query('SELECT SIND.cod_contable, SIND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria JOIN cod_cont_subindicador SIND ON IND.id_indicador = SIND.id_indicador WHERE CAT.id_categoria = 2;', (err, results) => {
     if (err) throw err;
     res.send(results);
   });
 });
 /* **************************************************************************************************************************************************************** */
-
+/* 
 /* **************************************************************************************************************************************************************** */
 /* Patrimonio */
 app.get('/categorias/patrimonio', (req, res) => {
   connection.query('SELECT * FROM `cod_cont_categoria` WHERE id_categoria = 3;', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.get('/subcategorias/patrimonio', (req, res) => {
+  connection.query('SELECT * FROM cod_cont_subcategoria WHERE id_categoria = 3;', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.get('/indicadores/patrimonio', (req, res) => {
+  connection.query('SELECT IND.cod_contable, IND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria WHERE CAT.id_categoria = 3;', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.get('/subindicadores/patrimonio', (req, res) => {
+  connection.query('SELECT SIND.cod_contable, SIND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria JOIN cod_cont_subindicador SIND ON IND.id_indicador = SIND.id_indicador WHERE CAT.id_categoria = 3;', (err, results) => {
     if (err) throw err;
     res.send(results);
   });
@@ -204,6 +231,24 @@ app.get('/categorias/ingresos', (req, res) => {
     res.send(results);
   });
 });
+app.get('/subcategorias/ingresos', (req, res) => {
+  connection.query('SELECT * FROM cod_cont_subcategoria WHERE id_categoria = 4;', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.get('/indicadores/ingresos', (req, res) => {
+  connection.query('SELECT IND.cod_contable, IND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria WHERE CAT.id_categoria = 4;', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.get('/subindicadores/ingresos', (req, res) => {
+  connection.query('SELECT SIND.cod_contable, SIND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria JOIN cod_cont_subindicador SIND ON IND.id_indicador = SIND.id_indicador WHERE CAT.id_categoria = 4;', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
 /* **************************************************************************************************************************************************************** */
 
 /* **************************************************************************************************************************************************************** */
@@ -214,6 +259,26 @@ app.get('/categorias/egresos', (req, res) => {
     res.send(results);
   });
 });
+app.get('/subcategorias/egresos', (req, res) => {
+  connection.query('SELECT * FROM cod_cont_subcategoria WHERE id_categoria = 5;', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.get('/indicadores/egresos', (req, res) => {
+  connection.query('SELECT IND.cod_contable, IND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria WHERE CAT.id_categoria = 5;', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.get('/subindicadores/egresos', (req, res) => {
+  connection.query('SELECT SIND.cod_contable, SIND.descripcion FROM cod_cont_categoria CAT JOIN cod_cont_subcategoria SC ON CAT.id_categoria = SC.id_categoria JOIN cod_cont_indicador IND ON SC.id_subcategoria = IND.id_subcategoria JOIN cod_cont_subindicador SIND ON IND.id_indicador = SIND.id_indicador WHERE CAT.id_categoria = 5;', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+/* **************************************************************************************************************************************************************** */
+
 
 
 
